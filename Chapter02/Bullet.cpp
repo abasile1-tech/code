8692dbd642085b0ba211ae54d2db92a54c5f9719
Ashton Basile <ashton.basile@gmail.com>
2021-02-24 09:57:52 -0700

    the beginning stages of adding a Bullet class by copying code from the Ship class.

---
+++ Chapter02/Bullet.cpp
@@ -0,0 +1,79 @@
+#include "Bullet.h"
+#include "AnimSpriteComponent.h"
+#include "Game.h"
+
+// the Ship constructor initializes mRightSpeed and mDownSpeed to 0,
+// and also creates an AnimSpriteComponent attached to the ship, 
+// with associated textures.
+Bullet::Bullet(Game* game)
+	:Actor(game)
+	, mRightSpeed(0.0f)
+	, mDownSpeed(0.0f)
+{
+	// Create an animated sprite component
+	AnimSpriteComponent* asc = new AnimSpriteComponent(this);
+	std::vector<SDL_Texture*> anims = {
+		game->GetTexture("Assets/Ship01.png"),
+		game->GetTexture("Assets/Ship02.png"),
+		game->GetTexture("Assets/Ship03.png"),
+		game->GetTexture("Assets/Ship04.png"),
+	};
+	asc->SetAnimTextures(anims);
+}
+
+// implements the bullet's movement
+void Bullet::UpdateActor(float deltaTime)
+{
+	Actor::UpdateActor(deltaTime);
+	// Update position based on speeds and delta time
+	Vector2 pos = GetPosition();
+	pos.x += mRightSpeed * deltaTime;
+	pos.y += mDownSpeed * deltaTime;
+	// Restrict position to left half of screen
+	if (pos.x < 25.0f)
+	{
+		pos.x = 25.0f;
+	}
+	else if (pos.x > 500.0f)
+	{
+		pos.x = 500.0f;
+	}
+	if (pos.y < 25.0f)
+	{
+		pos.y = 25.0f;
+	}
+	else if (pos.y > 743.0f)
+	{
+		pos.y = 743.0f;
+	}
+	SetPosition(pos);
+}
+
+// updates mRightSpeed and mDownSpeed based on the keyboard input from W, A, S, D, keys
+void Bullet::ProcessKeyboard(const uint8_t* state)
+{
+	mRightSpeed = 0.0f;
+	mDownSpeed = 0.0f;
+	// right/left
+	// D key moves the ship right
+	if (state[SDL_SCANCODE_D])
+	{
+		mRightSpeed += 250.0f;
+	}
+	// A key moves the ship left
+	if (state[SDL_SCANCODE_A])
+	{
+		mRightSpeed -= 250.0f;
+	}
+	// up/down
+	// S key moves the ship down
+	if (state[SDL_SCANCODE_S])
+	{
+		mDownSpeed += 300.0f;
+	}
+	// W key moves the ship up
+	if (state[SDL_SCANCODE_W])
+	{
+		mDownSpeed -= 300.0f;
+	}
+}
